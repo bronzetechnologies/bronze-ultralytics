fd854b50dac628d4fd885a58fe3b856ca095e3c4
Javi Esmoris <javier@bronze.vision>
2025-06-03 13:52:48 +0200

    comment second smart load

--- ultralytics/engine/model.py
+++ ultralytics/engine/model.py
@@ -813,7 +813,7 @@ class Model(torch.nn.Module):
         if overrides.get('resume'):
             overrides['resume'] = self.ckpt_path
         self.task = overrides.get('task') or self.task
-        trainer = trainer or self.smart_load('trainer')
+        # trainer = trainer or self.smart_load('trainer') ???
         self.trainer = self.task_map[self.task]['trainer'](overrides=overrides, _callbacks=self.callbacks, pipe_logger=self.pipe_logger)
         if not overrides.get('resume'):  # manually set model only if not resuming
             self.trainer.model = self.trainer.get_model(weights=self.model if self.ckpt else None, cfg=self.model.yaml)
